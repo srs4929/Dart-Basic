582ce794f29a1097814f8200667d93fe7c508e24
srs4929 <rahmansoma2003@gmail.com>
2025-03-27 16:34:09 +0600

    Variable

---
+++ variable.dart
@@ -0,0 +1,22 @@
+void main() {
+  var x = 90;
+  print(x);
+  int y = 78;
+  print(y);
+  var name = "Soma";
+  String n = "Rahman";
+  print(name + " " + n);
+
+  //dynamic
+  dynamic p = "Tim";
+  print(p);
+  p = "Tom";
+  print("Changed p " + p);
+
+  //const and final
+  const int t = 89;
+  print(t);
+  //declaring but not assigning
+  var ok;
+  print(ok);
+}
