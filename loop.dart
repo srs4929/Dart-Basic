f2744f319ed61d3a0cc73d36f70d8a13682e9935
srs4929 <rahmansoma2003@gmail.com>
2025-03-27 21:52:04 +0600

    loop

---
+++ loop.dart
@@ -0,0 +1,21 @@
+void main() {
+  var num = 5;
+  for (var i = 0; i < num; i++) {
+    print("Learning dart");
+  }
+  //var in loop
+  var names = ["Mango", "Orange", "Apple"];
+  for (var name in names) {
+    print(name);
+  }
+  //while loop
+  while (num >= 1) {
+    print(num);
+    num--;
+  }
+
+  var x = ["Sumaiya","Rahman","Soma"];
+  for (var y in x) {
+    print(y);
+  }
+}
