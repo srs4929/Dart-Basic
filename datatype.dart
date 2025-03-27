bc26121342ae51d8250c30ae5da93045603b9e16
srs4929 <rahmansoma2003@gmail.com>
2025-03-27 16:42:38 +0600

    Datatype done

---
+++ datatype.dart
@@ -0,0 +1,19 @@
+void main() {
+  //String
+  String x = "Soma";
+  print("The name : $x");
+  //Integer
+  int y = 90;
+  print("The value : $y");
+  //Doubles
+  double t = 90.97;
+  print("The double : $t");
+
+  //boolean
+  bool mybool = true;
+  print("My boolean value: $mybool");
+
+  //dynamic
+  dynamic test = "Ok";
+  print("Test is :$test");
+}
